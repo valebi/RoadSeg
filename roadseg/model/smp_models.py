19015c2aeabdf4bf3d3b805e1dcdedbe3191182c
valebi <bieriv@student.ethz.ch>
2023-06-22 00:16:10 +0200

    changed default decoder channels, added TODO

--- roadseg/model/smp_models.py
+++ roadseg/model/smp_models.py
@@ -2,26 +2,28 @@ import segmentation_models_pytorch as smp
 import torch
 from torch import nn
 
+
 def build_model(CFG, num_classes):
     if CFG.smp_model != "Unet":
         raise NotImplementedError(f"Model {CFG.smp_model} not implemented.")
-    
+
+    # @TODO: the UNet does not contract fully with depth=4. But if it does we need img_size % 32 == 0
     model = smp.Unet(
-        encoder_name=CFG.smp_backbone,      # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
-        encoder_weights=None, #"imagenet",     # use `imagenet` pre-trained weights for encoder initialization
-        in_channels=3,                  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
-        classes=num_classes,        # model output channels (number of classes in your dataset)
+        encoder_name=CFG.smp_backbone,  # choose encoder, e.g. mobilenet_v2 or efficientnet-b7
+        encoder_weights=None,  # "imagenet",     # use `imagenet` pre-trained weights for encoder initialization
+        in_channels=3,  # model input channels (1 for gray-scale images, 3 for RGB, etc.)
+        classes=num_classes,  # model output channels (number of classes in your dataset)
         activation=None,
-        decoder_channels=(64, 64, 32, 16),
+        decoder_channels=(128, 64, 32, 16),
         encoder_depth=4,
     )
     model.to(CFG.device)
-    model = nn.DataParallel(model) # Why does this hang? migrate to newer torch version
+    model = nn.DataParallel(model)
     return model
+
 
 def load_model(path):
     model = build_model()
     model.load_state_dict(torch.load(path))
     model.eval()
     return model
-
