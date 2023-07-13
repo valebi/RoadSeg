7a60d3e8c8507ad6946c7eeb2d8b0a07ea23c987
valebi <bieriv@student.ethz.ch>
2023-07-13 12:49:33 +0200

    added smp_model=UnetPlusPlus as argument option

--- roadseg/utils/args.py
+++ roadseg/utils/args.py
@@ -123,6 +123,7 @@ def parse_args() -> argparse.Namespace:
         "--smp_model",
         type=str,
         default="Unet",
+        choices=["Unet", "UnetPlusPlus", "DeepLabV3"],
         help="Model (/Framework) for pytorch-segmentation-models",
     )
     parser.add_argument(
