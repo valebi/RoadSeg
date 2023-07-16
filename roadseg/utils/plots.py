9efe6d595170989ca3e58fa68e11a5c6f5d13878
OnatV <onat.project@gmail.com>
2023-07-16 16:29:22 +0200

    Fixed open figures bug better

--- roadseg/utils/plots.py
+++ roadseg/utils/plots.py
@@ -74,4 +74,4 @@ def plot_batch(imgs, msks, pred=None, num_cols=5, src="", log_dir=None):
             fig.savefig(os.path.join(log_dir, f"sample_pred_{src}.png"))
 
     # plt.show()
-    plt.close(fig)
+    plt.close("all")
