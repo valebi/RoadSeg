f30dca63f2728ee07b093a74b6e6e80efc2a27a7
valebi <bieriv@student.ethz.ch>
2023-07-24 21:20:20 +0200

    fix

--- roadseg/train_single_ds_diffusion.py
+++ roadseg/train_single_ds_diffusion.py
@@ -59,7 +59,7 @@ def train_one_epoch(
             # y_pred = model(images)
             # make labels one-hot
             onehot_labels = torch.cat([1 - labels[:, None], labels[:, None]], dim=1)
-            loss = diffusion(onehot_labels.float(), images)
+            loss = diffusion(onehot_labels.to(torch.float32), images)
             # y_pred = y_pred * loss_mask[:, None]
             # loss = criterion(y_pred[:, 1], labels.float())
             loss = loss / n_accumulate
