d670245f075732341eed85b77529c1aa8428b48b
valebi <bieriv@student.ethz.ch>
2023-07-27 10:10:55 +0200

    made diffusion optional

--- main.py
+++ main.py
@@ -9,9 +9,11 @@ from torch import nn
 from torchsummary import summary
 
 from roadseg.datasets.dataloaders import get_dataloaders
-from roadseg.inference_diffusion import make_ensemble, make_submission
 from roadseg.model.smp_models import build_model
-from roadseg.train_single_ds_diffusion import evaluate_finetuning, pretrain_model
+
+# @TODO fix imports
+# from roadseg.train_single_ds_diffusion import evaluate_finetuning, pretrain_model
+# from roadseg.inference_diffusion import make_ensemble, make_submission
 from roadseg.utils.augmentations import get_albumentations
 from roadseg.utils.plots import plot_batch
 from roadseg.utils.utils import download_file_from_google_drive, finalize, setup
@@ -19,6 +21,13 @@ from roadseg.utils.utils import download_file_from_google_drive, finalize, setup
 
 def main(CFG: Namespace):
     """Main function."""
+    if CFG.use_diffusion:
+        from roadseg.inference_diffusion import make_ensemble, make_submission
+        from roadseg.train_single_ds_diffusion import evaluate_finetuning, pretrain_model
+    else:
+        from roadseg.inference import make_ensemble, make_submission
+        from roadseg.train_single_ds import evaluate_finetuning, pretrain_model
+
     start = time.time()
 
     transforms = get_albumentations(CFG)
